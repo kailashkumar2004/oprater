475f8604a575f1212f5d26b3f2cf7cb28b7fe609
kailash <kailashkumartkg@gmail.com>
2023-07-27 21:30:06 +0530

    initial commit

---
+++ assignment.oprater.js
@@ -0,0 +1,29 @@
+// var a=77
+// var b=88
+// console.log(a=b)
+// console.log(a+=b)
+// console.log(a-=b)
+// console.log(a*=b)
+// console.log(a/=b)
+// console.log(a%=b)
+// console.log(a**=b)
+
+// var a=99
+// var b=100
+// console.log(a)
+// console.log(a+=b)
+// console.log("-----",a-=b)
+// console.log("****",a*=b)
+// console.log(a/=b)
+// console.log(a%=b)
+// console.log(a**=b)
+
+var a=33
+var b=22
+console.log(a)
+console.log(a+=b)
+console.log(a-=b)
+console.log(a*=b)
+console.log(a/=b)
+console.log(a%=b)
+console.log(a**=b)
